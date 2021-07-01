b606beeb496ad3d4c2a3f11446569a10b4dd2aaf
Jason Costello <email@jasoncostello.com>
2021-07-01 18:01:29 -0500

    header section built

--- read_test.go
+++ read_test.go
@@ -1,21 +1,22 @@
 package gedworx
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
-
-func getTestFiles(path string) ([]string, error){
+func getTestFiles(path string) ([]string, error) {
 
 	var fileNames []string
 	files, err := ioutil.ReadDir(path)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-	for _, f := range  files {
+	for _, f := range files {
 		if !f.IsDir() {
 			fileNames = append(fileNames, f.Name())
 		}
@@ -28,17 +29,15 @@ func getTestFiles(path string) ([]string, error){
 func TestReadAll(t *testing.T) {
 	var testFiles []string
 	var err error
-	if testFiles, err  = getTestFiles("./testdata"); err != nil{
+	if testFiles, err = getTestFiles("./testdata"); err != nil {
 		t.Fatalf(err.Error())
 	}
-
-
 
 	a := assert.New(t)
 	type args struct {
 		filePath string
 	}
-	type test struct  {
+	type test struct {
 		name    string
 		args    args
 		want    []byte
@@ -46,14 +45,15 @@ func TestReadAll(t *testing.T) {
 	}
 
 	var tests []test
-	for _, testFile := range testFiles{
+	for _, testFile := range testFiles {
 
 		tests = append(tests, test{
-			name: testFile,
-			args: args{filePath: fmt.Sprintf("./testdata/%s", testFile)},
-			want: []byte{},
+			name:    testFile,
+			args:    args{filePath: fmt.Sprintf("./testdata/%s", testFile)},
+			want:    []byte{},
 			wantErr: false,
 		})
+
 	}
 
 	for _, tt := range tests {
@@ -64,7 +64,103 @@ func TestReadAll(t *testing.T) {
 				return
 			}
 
-				a.IsType(tt.want, got)
+			a.IsType(tt.want, got)
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
+	a := assert.New(t)
+
+	var testFiles []string
+	var err error
+	if testFiles, err = getTestFiles("./testdata"); err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	type args struct {
+		input []byte
+	}
+	type test struct {
+		name    string
+		args    args
+		wantErr bool
+	}
+	var tests []test
+
+	for _, testFile := range testFiles {
+
+		tests = append(tests, test{
+			name:    fmt.Sprintf("./testdata/%s", testFile),
+			args:    args{},
+			wantErr: !strings.Contains(testFile, "utf8"),
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.args.input, err = ReadAll(tt.name)
+			if err != nil {
+				t.Fail()
+				return
+			}
+			err = Parse(tt.args.input)
+			if err != nil {
+				if a.Error(err) != tt.wantErr {
+					t.Fail()
+					return
+				}
+			}
+
+			// a.NotEqual(tt.want, a.NoError(err ))
+
+		})
+	}
+}
+func Test_HasUTF8BOM(t *testing.T) {
+	a := assert.New(t)
+	var testFiles []string
+	var err error
+	if testFiles, err = getTestFiles("./testdata"); err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	type args struct {
+		firstLine []byte
+	}
+	type test struct {
+		name string
+		args args
+		want bool
+	}
+	var tests []test
+
+	for _, testFile := range testFiles {
+
+		tests = append(tests, test{
+			name: fmt.Sprintf("./testdata/%s", testFile),
+			args: args{},
+			want: strings.Contains(testFile, "utf8"),
+		})
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			input, err := ReadAll(tt.name)
+			if err != nil {
+				t.Log(fmt.Sprintf("%s:  %s ", tt.name, err.Error()))
+				t.Fail()
+				return
+			}
+			scanner := bufio.NewScanner(strings.NewReader(string(input)))
+
+			for scanner.Scan() {
+				temp := scanner.Text()
+
+				a.Equal(tt.want, HasUTF8BOM(temp))
+				break
+			}
+
 		})
 	}
 }
